b607688c78ea6de871b3471a74a769fb101221d7
scrooge <etoerik@gmail.com>
2020-08-02 12:25:04 +0300

    add catch for user

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,23 @@
 const userRouter = require('express').Router();
+const fsPromises = require('fs').promises;
 const path = require('path');
 
 userRouter.get('/users/:id', (req, res) => {
   const dataPath = path.join(__dirname, '../data/users.json');
-  const users = require(dataPath);
 
-  const user = users.filter((item) => item._id === req.params.id);
-
-  if (user.length === 0) {
-    res.status(404);
-    res.contentType('JSON');
-    res.send({ error: 'Такого пользователя нет' });
-    return;
-  }
-
-  res.status(200);
-  res.contentType('JSON');
-  res.send(user);
+  fsPromises.readFile(dataPath, { encoding: 'utf8' })
+    .then((data) => {
+      const users = JSON.parse(data);
+      const user = users.filter((item) => item._id === req.params.id);
+      res.status(200);
+      res.contentType('JSON');
+      res.send(user);
+    })
+    .catch(() => {
+      res.status(500);
+      res.contentType('JSON');
+      res.send({ message: 'Внутренняя ошибка сервера' });
+    });
 });
 
 module.exports = userRouter;
